d138361e1f5aa08fabcf8e5a095d3e767c0ece20
druvkotwani <druvkotwani12@gmail.com>
2023-11-23 13:38:23 +0530

    .env added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"; // Add Firestore module impor
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.measurementId,
-    measurementId: process.env.appId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
