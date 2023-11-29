e6195ba75276bcec78f4b723169d6474b8f10d8b
druvkotwani <druvkotwani12@gmail.com>
2023-11-29 10:35:58 +0530

    some styling changed in about.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,36 +8,36 @@ const About = ({ result }) => {
                 <span className='text-sm text-[#BDBEC3] '>India</span>
             </p> */}
             {
-                result?.website?.link && (<p className="flex">
+                result?.website?.link && (<a target="_blank" rel='noreferrer' href={result?.website?.link} className="flex">
                     <iconify-icon icon="streamline-emojis:globe-showing-americas" style={{ color: 'white', marginRight: '1px' }} width="19" height="19"></iconify-icon>
                     <span className='text-sm text-[#BDBEC3] '>
-                        <a href={result?.website?.link} target="_blank" rel='noreferrer' className="">{result?.website?.text}</a>
+                        <p className="">{result?.website?.text}</p>
                     </span>
-                </p>)
+                </a>)
             }
             {
-                result?.github?.link && (<p className="flex">
+                result?.github?.link && (<a target="_blank" rel='noreferrer' href={result?.github?.link} className="flex">
                     <iconify-icon icon="mdi:github" style={{ color: 'white', marginRight: '5px' }} width="17" height="19"></iconify-icon>
                     <span className='text-sm text-[#BDBEC3] '>
-                        <a href={result?.github?.link} target="_blank" rel='noreferrer' className="">{result?.github?.text}</a>
+                        <p className="">{result?.github?.text}</p>
                     </span>
-                </p>)
+                </a>)
             }
             {
-                result?.twitter?.link && (<p className="flex">
+                result?.twitter?.link && (<a target="_blank" rel='noreferrer' href={result?.twitter?.link} className="flex">
                     <iconify-icon icon="fa6-brands:square-x-twitter" style={{ color: 'white', marginRight: '5px' }} width="17" height="19"></iconify-icon>
                     <span className='text-sm text-[#BDBEC3] '>
-                        <a href={result?.twitter?.link} target="_blank" rel='noreferrer' className="">{result?.twitter?.text}</a>
+                        <p className="">{result?.twitter?.text}</p>
                     </span>
-                </p>)
+                </a>)
             }
             {
-                result?.linkedin?.link && (<p className="flex">
+                result?.linkedin?.link && (<a target="_blank" rel='noreferrer' href={result?.linkedin?.link} className="flex">
                     <iconify-icon icon="devicon:linkedin" style={{ color: 'white', marginRight: '5px' }} width="17" height="19  "></iconify-icon>
                     <span className='text-sm text-[#BDBEC3] '>
-                        <a href={result?.linkedin?.link} target="_blank" rel='noreferrer' className="">{result?.linkedin?.text}</a>
+                        <p className="">{result?.linkedin?.text}</p>
                     </span>
-                </p>
+                </a>
                 )
             }
         </div>
