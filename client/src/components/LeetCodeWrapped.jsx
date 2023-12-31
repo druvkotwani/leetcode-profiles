6300041ddec58072888e3de720a995a4f01b8976
druvkotwani <druvkotwani12@gmail.com>
2023-12-31 13:53:37 +0530

    text changed

--- client/src/components/LeetCodeWrapped.jsx
+++ client/src/components/LeetCodeWrapped.jsx
@@ -64,7 +64,7 @@ const LeetCodeWrapped = () => {
 
             <div className=' flex items-center justify-center'>
                 <input type="text" placeholder="Generate Your LeetCode-Wrap" className="my-2 w-[250px] input input-bordered input-warning " />
-                <button className="btn btn-success ml-3">Generate</button>
+                <button className="btn btn-outline btn-accent ml-3">Generate</button>
             </div>
 
             {/* Community Stat */}
@@ -111,7 +111,7 @@ const LeetCodeWrapped = () => {
                             <div className="stat-figure text-primary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             </div>
-                            <div className="stat-title">Total Likes</div>
+                            <div className="stat-title">Solutions</div>
                             <div className="stat-value text-primary">25.6K</div>
                             <div className="stat-desc">Last Week: 0</div>
                         </div>
