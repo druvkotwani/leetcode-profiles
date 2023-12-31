25d14b75db7db8b645277e0c7fd104520805814c
druvkotwani <druvkotwani12@gmail.com>
2023-12-31 14:26:39 +0530

    data added

--- client/src/components/LeetCodeWrapped.jsx
+++ client/src/components/LeetCodeWrapped.jsx
@@ -7,6 +7,21 @@ const LeetCodeWrapped = () => {
 
     const [activeButton, setActiveButton] = useState('profile');
 
+    const tempData = {
+        fullName: "Dhruv Kotwani",
+        imageUrl: "/assets/profile.png",
+        totalQuestions: 3013,
+        totalSolved: 1072,
+        Views: "621",
+        Solution: "9",
+        Reputation: "22",
+        submissions: "950",
+        activeDays: "214",
+        maxStreak: "145"
+    }
+
+    const questionsDone = Math.floor(tempData.totalSolved / tempData.totalQuestions * 100);
+    const questionsRemain = 100 - questionsDone;
 
     const handleClick = (button) => {
         if (button !== activeButton) {
@@ -15,7 +30,6 @@ const LeetCodeWrapped = () => {
     };
     return (
         <div className='h-screen w-screen'>
-
             {/* Header*/}
             <div className="flex sm:gap-4 items-center justify-center p-4">
                 {/* SVG */}
@@ -69,17 +83,17 @@ const LeetCodeWrapped = () => {
 
             {/* Community Stat */}
             <div className='flex items-center justify-center '>
-                <div className="stats shadow flex sm:flex overflow-hidden  sm:w-[580px]  items-center justify-center rounded-none">
+                <div className="stats shadow flex sm:flex overflow-hidden  sm:w-[550px]  items-center justify-center rounded-none">
                     <div className='flex        '>
                         <div className="stat ">
                             <div className="stat-figure text-primary">
                             </div>
                             <div className="stat-title ">Hey!</div>
-                            <div className="stat-value text-[#FF8866]">Dhruv</div>
+                            <div className="stat-value text-[#FF8866]">{tempData.fullName.split(' ')[0]}</div>
                             <div className="stat-desc">How was your 2023?</div>
                         </div>
 
-                        <div className="stat ">
+                        <div className="stat pl-3">
                             <div className="stat-figure text-secondary">
                                 <div className="avatar">
                                     <div className="w-16 rounded-full">
@@ -87,9 +101,9 @@ const LeetCodeWrapped = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="stat-value">86%</div>
+                            <div className="stat-value">{questionsDone}%</div>
                             <div className="stat-title">Questions Done</div>
-                            <div className="stat-desc text-[secondary]">31% Questions remains</div>
+                            <div className="stat-desc text-[secondary]">{questionsRemain}% Questions remains</div>
                         </div>
                     </div>
                 </div>
@@ -97,38 +111,38 @@ const LeetCodeWrapped = () => {
 
             <div className="flex sm:flex-col items-center justify-center">
                 {/* Div 2 */}
-                <div className="stats shadow flex sm:flex-col overflow-hidden  items-center justify-center rounded-none">
+                <div className="stats shadow flex sm:flex-col overflow-hidden sm:w-[550px]  items-center justify-center rounded-none">
                     <div className='sm:flex '>
                         <div className="stat">
                             <div className="stat-figure text-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                             </div>
                             <div className="stat-title">Profile Views</div>
-                            <div className="stat-value text-secondary">2.6M</div>
-                            <div className="stat-desc">Last Week: 0</div>
+                            <div className="stat-value text-secondary">{tempData.Views}</div>
+                            <div className="stat-desc">Total Profile Views</div>
                         </div>
                         <div className="stat">
                             <div className="stat-figure text-primary">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                             </div>
                             <div className="stat-title">Solutions</div>
-                            <div className="stat-value text-primary">25.6K</div>
-                            <div className="stat-desc">Last Week: 0</div>
+                            <div className="stat-value text-primary">{tempData.Solution}</div>
+                            <div className="stat-desc">Total Solutions</div>
                         </div>
                         <div className="stat">
                             <div className="stat-figure text-primary">
                                 <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11.2691 4.41115C11.5006 3.89177 11.6164 3.63208 11.7776 3.55211C11.9176 3.48263 12.082 3.48263 12.222 3.55211C12.3832 3.63208 12.499 3.89177 12.7305 4.41115L14.5745 8.54808C14.643 8.70162 14.6772 8.77839 14.7302 8.83718C14.777 8.8892 14.8343 8.93081 14.8982 8.95929C14.9705 8.99149 15.0541 9.00031 15.2213 9.01795L19.7256 9.49336C20.2911 9.55304 20.5738 9.58288 20.6997 9.71147C20.809 9.82316 20.8598 9.97956 20.837 10.1342C20.8108 10.3122 20.5996 10.5025 20.1772 10.8832L16.8125 13.9154C16.6877 14.0279 16.6252 14.0842 16.5857 14.1527C16.5507 14.2134 16.5288 14.2807 16.5215 14.3503C16.5132 14.429 16.5306 14.5112 16.5655 14.6757L17.5053 19.1064C17.6233 19.6627 17.6823 19.9408 17.5989 20.1002C17.5264 20.2388 17.3934 20.3354 17.2393 20.3615C17.0619 20.3915 16.8156 20.2495 16.323 19.9654L12.3995 17.7024C12.2539 17.6184 12.1811 17.5765 12.1037 17.56C12.0352 17.5455 11.9644 17.5455 11.8959 17.56C11.8185 17.5765 11.7457 17.6184 11.6001 17.7024L7.67662 19.9654C7.18404 20.2495 6.93775 20.3915 6.76034 20.3615C6.60623 20.3354 6.47319 20.2388 6.40075 20.1002C6.31736 19.9408 6.37635 19.6627 6.49434 19.1064L7.4341 14.6757C7.46898 14.5112 7.48642 14.429 7.47814 14.3503C7.47081 14.2807 7.44894 14.2134 7.41394 14.1527C7.37439 14.0842 7.31195 14.0279 7.18708 13.9154L3.82246 10.8832C3.40005 10.5025 3.18884 10.3122 3.16258 10.1342C3.13978 9.97956 3.19059 9.82316 3.29993 9.71147C3.42581 9.58288 3.70856 9.55304 4.27406 9.49336L8.77835 9.01795C8.94553 9.00031 9.02911 8.99149 9.10139 8.95929C9.16534 8.93081 9.2226 8.8892 9.26946 8.83718C9.32241 8.77839 9.35663 8.70162 9.42508 8.54808L11.2691 4.41115Z" stroke="#FCD53F" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
                             </div>
                             <div className="stat-title ">Reputation</div>
-                            <div className="stat-value text-[#FCD53F]">25.6K</div>
-                            <div className="stat-desc">Last Week: 0</div>
+                            <div className="stat-value text-[#FCD53F]">{tempData.Reputation}</div>
+                            <div className="stat-desc">Total Reputataion</div>
                         </div>
                     </div>
                 </div>
 
 
                 {/* Div 3 */}
-                <div className="stats shadow flex sm:flex-col overflow-hidden  items-center justify-center rounded-none ">
+                <div className="stats shadow flex sm:flex-col overflow-hidden sm:w-[550px] items-center justify-center rounded-none ">
                     <div className='sm:flex '>
                         <div className="stat">
                             <div className="stat-figure text-secondary">
@@ -153,7 +167,7 @@ const LeetCodeWrapped = () => {
                                 </svg>
                             </div>
                             <div className="stat-title ">Submissions</div>
-                            <div className="stat-value text-[#03AAEE]">2.6M</div>
+                            <div className="stat-value text-[#03AAEE]">{tempData.Solution}</div>
                             <div className="stat-desc">Total Submission</div>
                         </div>
                         <div className="stat">
@@ -176,9 +190,10 @@ const LeetCodeWrapped = () => {
                                             <path d="M8.6,20.1l-7.8-8l1.4-1.4l6.4,6.5L21.8,3.9l1.4,1.4L8.6,20.1z"></path>
                                         </g>
                                     </g>
-                                </svg>                            </div>
+                                </svg>
+                            </div>
                             <div className="stat-title">Active Days</div>
-                            <div className="stat-value text-[#00A96E]">25.6K</div>
+                            <div className="stat-value text-[#00A96E]">{tempData.activeDays}</div>
                             <div className="stat-desc">Total Active Days</div>
                         </div>
                         <div className="stat">
@@ -204,7 +219,7 @@ const LeetCodeWrapped = () => {
                                 </svg>
                             </div>
                             <div className="stat-title ">Max Streak</div>
-                            <div className="stat-value text-[#B2F302]">25.6K</div>
+                            <div className="stat-value text-[#B2F302]">{tempData.maxStreak}</div>
                             <div className="stat-desc">Overall Max Streak</div>
                         </div>
                     </div>
