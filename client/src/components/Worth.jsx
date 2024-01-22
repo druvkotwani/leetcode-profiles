46352abe34d74a30ab57f5095ea8934a0fa5c4b6
druvkotwani <druvkotwani12@gmail.com>
2024-01-22 17:08:17 +0530

    worth alert, home page link added

--- client/src/components/Worth.jsx
+++ client/src/components/Worth.jsx
@@ -3,7 +3,7 @@ import About from './About'
 import data from '../utils/tempdata'
 import Profile from './Profile'
 import SvgData from '../utils/svgData'
-import Questions from './Questions'
+import { Link } from 'react-router-dom';
 
 const Worth = () => {
     const [userName, setUserName] = useState('')
@@ -13,7 +13,7 @@ const Worth = () => {
         setUserName(e.target.value)
     }
     return (
-        <div className='h-full w-screen'>
+        <div className='h-full w-full'>
             {/* Header*/}
             <div className="flex gap-2 sm:gap-4 items-center justify-center p-4 ">
                 {/* icon */}
@@ -41,21 +41,30 @@ const Worth = () => {
                 <form onSubmit={(e) => { console.log(userName); e.preventDefault(); setLoading(true) }} className="flex flex-col justify-center items-center gap-4 p-4">
                     <input type="text" placeholder="Enter your LeetCode username" className="border-2 focus:border-yellow-500 focus:outline-none focus:ring focus:ring-yellow-200     border-gray-300 text-white rounded-md p-2 w-80 sm:w-96" onChange={handleInputChange} />
 
-                    <button disabled={loading} className={`${userName.trim() === '' ? 'pointer-events-none opacity-50 flex items-center justify-center gap-2' : ''}flex items-center justify-center gap-2  bg-yellow-500 text-white font-[poppins] font-medium text-sm sm:text-base rounded-md p-2 w-40 sm:w-46`} >
-                        {
-                            loading ?
-                                <svg className="animate-spin h-5 w-5 " viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="white" strokeWidth="4" />
-                                    <path className="opacity-75" fill="white" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
-                                </svg>
-                                :
-                                (<iconify-icon icon="noto-v1:money-with-wings" width='20' height='20' ></iconify-icon>)
-                        }
-                        {
-                            loading ? 'Calculating' : 'Calculate'
-                        }
-                    </button>
+                    <div className='flex gap-2 items-center justify-center'>
+                        <button disabled={loading} className={`${userName.trim() === '' ? 'pointer-events-none opacity-50 flex items-center justify-center gap-2' : ''}flex items-center justify-center gap-2  bg-yellow-500 text-white font-[poppins] font-medium text-sm sm:text-base rounded-md p-2 w-40 sm:w-46`} >
+                            {
+                                loading ?
+                                    <svg className="animate-spin h-5 w-5 " viewBox="0 0 24 24">
+                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="white" strokeWidth="4" />
+                                        <path className="opacity-75" fill="white" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z" />
+                                    </svg>
+                                    :
+                                    (<iconify-icon icon="noto-v1:money-with-wings" width='20' height='20' ></iconify-icon>)
+                            }
+                            {
+                                loading ? 'Calculating' : 'Calculate'
+                            }
+                        </button>
+                        <Link to='/'>
+                            <button className={`mx-auto flex items-center justify-center gap-2  bg-yellow-500 text-white font-[poppins] font-medium text-sm sm:text-base rounded-md p-2 `} >
+                                <iconify-icon icon="noto:house" width='20' height='20' ></iconify-icon>
+                            </button>
+                        </Link>
+                    </div>
                 </form>
+
+
             </div>
 
             {/* Body */}
@@ -118,9 +127,6 @@ const Worth = () => {
                 </button>
 
             </div>
-
-
-
         </div >
 
     )
---
+++ client/src/components/WorthAlert.jsx
@@ -0,0 +1,47 @@
+import '../App.css';
+import '../index.css'
+import { Link } from 'react-router-dom';
+const WorthAlert = () => {
+
+    const closeAlert = () => {
+        const alert = document.getElementById('alert');
+        alert.style.display = 'none';
+    };
+
+    return (
+        <div id="alert">
+            <span className="mr-4 text-white font-bold float-right text-2xl cursor-pointer transition duration-300" onClick={closeAlert}>
+                &times;
+            </span>
+            <div className="flex sm:gap-4 items-center justify-center">
+                <div className="flex gap-2 sm:gap-4 items-center justify-center p-4 ">
+                    {/* icon */}
+                    <div className='glowing-icon'>
+                        <iconify-icon icon="noto-v1:money-bag" width="62" height="62"></iconify-icon>
+                    </div>
+
+                    {/* Text */}
+                    <div className=" flex flex-col justify-center ">
+                        <h1 className="flex gap-2 font-[poppins] text-white font-medium text-2xl sm:text-3xl">
+                            LeetCode
+                            <span className='nunito-bold text-3xl sm:text-4xl text-yellow-400 cedarville'> Worth</span>
+                        </h1>
+
+                        <p className="hidden sm:block font-[poppins] text-gray-200 text-xs   sm:text-base ">Calculate the Real Worth of Your Hours Spent on DSA!</p>
+                    </div>
+                </div>
+                <div style={{ height: '0.5px', backgroundColor: '#E0E0E0' }} className="h-[0.5px] bg-white" />
+
+                {/* Button */}
+                <Link to="/leetcodeWorth">
+                    <button className="ml-1 btn btn-outline btn-warning sm:hidden">💸</button>
+                    <button className="ml-1 btn btn-outline btn-warning hidden sm:block">Explore💸</button>
+                </Link>
+
+            </div>
+
+        </div >
+    );
+};
+
+export default WorthAlert;
