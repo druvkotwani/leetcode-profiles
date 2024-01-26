ee3981e3720379100adb5f5c5cef8bb1f0518c6a
druvkotwani <druvkotwani12@gmail.com>
2024-01-26 12:57:13 +0530

    fixed bg using h-screen

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -86,7 +86,7 @@ export default function Home() {
                         </div>
                     )
                         : (
-                            <div className="flex flex-col mx-auto max-w-screen-xl px-2 sm:px-6 lg:px-8">
+                            <div className="min-h-screen flex flex-col mx-auto max-w-screen-xl px-2 sm:px-6 lg:px-8">
                                 <div className="grid grid-cols-1 gap-4 py-4 ">
                                     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                                         {
