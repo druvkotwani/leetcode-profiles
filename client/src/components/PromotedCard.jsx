874935ec68d4f8ed3529f10b8f66eaab949b7cd2
druvkotwani <druvkotwani12@gmail.com>
2024-03-01 11:09:28 +0530

    changed the image in promoted card

--- client/src/components/PromotedCard.jsx
+++ client/src/components/PromotedCard.jsx
@@ -1,3 +1,4 @@
+import { LeetcodeIcon } from "../utils/Icons";
 
 
 export default function PromotedCard({ option }) {
@@ -10,15 +11,8 @@ export default function PromotedCard({ option }) {
                 {/* Heading and Logo */}
                 <div className="flex gap-1 xl:gap-4 items-center justify-between   ">
                     <div className="flex items-center text-white justify-center px-4 py-2 w-full gap-2   rounded-md">
-                        <img
-                            alt="nextui logo"
-                            className="rounded-full"
-                            height={24}
-                            radius="sm"
-                            src={`/assets/leetcode.png`}
-                            width={24}
-                        />
-                        <h4 className="font-bold text-large SourceCodePro">LeetCode Profiles</h4>
+                        <LeetcodeIcon />
+                        <h4 className="font-bold text-xl SourceCodePro">LeetCode Profiles</h4>
                     </div>
                 </div>
 
@@ -26,7 +20,7 @@ export default function PromotedCard({ option }) {
                 <div className="">
                     <div style={{ height: '0.5px', backgroundColor: '#E0E0E0' }} className="h-[0.5px] bg-white" />
 
-                    <p className=" px-4 text-[#FFFEFE] mt-2 font-medium  text-base xl:text-lg  SourceCodePro">
+                    <p className=" px-4 text-[#FFFEFE] mt-2  text-base  SourceCodePro">
                         {option.heading}
                     </p>
 
